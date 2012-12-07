c1178306bdd5ba8608a5cfc1088e796ad2766528
Cheng Ning <cn2005nian@163.com>
2012-12-07 17:01:45 +0800

    rename system enum files

---
+++ Model/Enum/SysEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Enum
+{
+    /// <summary>
+    /// 系统类型，核心，销售等
+    /// </summary>
+    public enum SysEnum
+    {
+    }
+
+    /// <summary>
+    /// 渠道，团险，个险
+    /// </summary>
+    public enum ChannelEnum
+    { 
+    }
+
+    /// <summary>
+    /// 系统模块，新契约，保全，理赔
+    /// </summary>
+    public enum ModuleEnum
+    { 
+    }
+
+    /// <summary>
+    /// 应用更新表对应的sheet
+    /// </summary>
+    public enum RecordSheetEnum
+    {
+        UI=1,
+        JAVA=2,
+        SQL=3
+    }
+}
