4ec45ec2932d335239c796c87dbd93c7eb038bdb
Cheng Ning <cn2005nian@163.com>
2012-12-14 08:58:54 +0800

    添加windows FORM的界面，同时添加了界面设计工程（GUI design studio project）

---
+++ UIForm/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace UIForm
+{
+    partial class TaskSubmitor
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新建任务ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新建文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设计文档ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.自测报告ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.修改列表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dEVSQLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dDLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据库审批表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据库PDMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.用户配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.路径配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.自动化配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.环境配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.about = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(827, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建任务ToolStripMenuItem,
+            this.新建文件ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 新建任务ToolStripMenuItem
+            // 
+            this.新建任务ToolStripMenuItem.Name = "新建任务ToolStripMenuItem";
+            this.新建任务ToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.新建任务ToolStripMenuItem.Text = "新建任务";
+            // 
+            // 新建文件ToolStripMenuItem
+            // 
+            this.新建文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设计文档ToolStripMenuItem,
+            this.自测报告ToolStripMenuItem,
+            this.修改列表ToolStripMenuItem,
+            this.dEVSQLToolStripMenuItem,
+            this.dMLToolStripMenuItem,
+            this.dDLToolStripMenuItem,
+            this.数据库审批表ToolStripMenuItem,
+            this.数据库PDMToolStripMenuItem});
+            this.新建文件ToolStripMenuItem.Name = "新建文件ToolStripMenuItem";
+            this.新建文件ToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.新建文件ToolStripMenuItem.Text = "新建文件";
+            // 
+            // 设计文档ToolStripMenuItem
+            // 
+            this.设计文档ToolStripMenuItem.Name = "设计文档ToolStripMenuItem";
+            this.设计文档ToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.设计文档ToolStripMenuItem.Text = "设计文档";
+            // 
+            // 自测报告ToolStripMenuItem
+            // 
+            this.自测报告ToolStripMenuItem.Name = "自测报告ToolStripMenuItem";
+            this.自测报告ToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.自测报告ToolStripMenuItem.Text = "自测报告";
+            // 
+            // 修改列表ToolStripMenuItem
+            // 
+            this.修改列表ToolStripMenuItem.Name = "修改列表ToolStripMenuItem";
+            this.修改列表ToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.修改列表ToolStripMenuItem.Text = "修改列表";
+            // 
+            // dEVSQLToolStripMenuItem
+            // 
+            this.dEVSQLToolStripMenuItem.Name = "dEVSQLToolStripMenuItem";
+            this.dEVSQLToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.dEVSQLToolStripMenuItem.Text = "DEV-SQL";
+            // 
+            // dMLToolStripMenuItem
+            // 
+            this.dMLToolStripMenuItem.Name = "dMLToolStripMenuItem";
+            this.dMLToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.dMLToolStripMenuItem.Text = "DML";
+            // 
+            // dDLToolStripMenuItem
+            // 
+            this.dDLToolStripMenuItem.Name = "dDLToolStripMenuItem";
+            this.dDLToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.dDLToolStripMenuItem.Text = "DDL";
+            // 
+            // 数据库审批表ToolStripMenuItem
+            // 
+            this.数据库审批表ToolStripMenuItem.Name = "数据库审批表ToolStripMenuItem";
+            this.数据库审批表ToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.数据库审批表ToolStripMenuItem.Text = "数据库审批表";
+            // 
+            // 数据库PDMToolStripMenuItem
+            // 
+            this.数据库PDMToolStripMenuItem.Name = "数据库PDMToolStripMenuItem";
+            this.数据库PDMToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.数据库PDMToolStripMenuItem.Text = "数据库PDM";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.用户配置ToolStripMenuItem,
+            this.路径配置ToolStripMenuItem,
+            this.自动化配置ToolStripMenuItem,
+            this.环境配置ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 用户配置ToolStripMenuItem
+            // 
+            this.用户配置ToolStripMenuItem.Name = "用户配置ToolStripMenuItem";
+            this.用户配置ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.用户配置ToolStripMenuItem.Text = "用户配置";
+            // 
+            // 路径配置ToolStripMenuItem
+            // 
+            this.路径配置ToolStripMenuItem.Name = "路径配置ToolStripMenuItem";
+            this.路径配置ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.路径配置ToolStripMenuItem.Text = "路径配置";
+            // 
+            // 自动化配置ToolStripMenuItem
+            // 
+            this.自动化配置ToolStripMenuItem.Name = "自动化配置ToolStripMenuItem";
+            this.自动化配置ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.自动化配置ToolStripMenuItem.Text = "自动化配置";
+            // 
+            // 环境配置ToolStripMenuItem
+            // 
+            this.环境配置ToolStripMenuItem.Name = "环境配置ToolStripMenuItem";
+            this.环境配置ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.环境配置ToolStripMenuItem.Text = "环境配置";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.about});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // about
+            // 
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(152, 22);
+            this.about.Text = "关于";
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 316);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(155, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(660, 315);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 349);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(815, 181);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // TaskSubmitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 542);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TaskSubmitor";
+            this.Text = "任务提交器";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新建任务ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新建文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设计文档ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 自测报告ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 修改列表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dEVSQLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dDLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 数据库审批表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 数据库PDMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 用户配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 路径配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 自动化配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 环境配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem about;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ UIForm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UIForm
+{
+    public partial class TaskSubmitor : Form
+    {
+        public TaskSubmitor()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, EventArgs e)
+        {
+            AboutBox ab = new AboutBox();
+            ab.Show();
+        }
+
+    }
+}
