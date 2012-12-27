9daf75b95a46216e602f014f75a2cfe5316fb15d
Cheng Ning <cn2005nian@163.com>
2012-12-27 16:44:08 +0800

    lib for operation vss

---
+++ VssHelper/VssHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VssHelper
+{
+    /// <summary>
+    /// 说明：提供vss操作的接口,可以作为单独的DLL发布
+    /// 作者：ChengNing
+    /// 日期：2012-12-27
+    /// </summary>
+    public class VssHelper
+    {
+    }
+}
