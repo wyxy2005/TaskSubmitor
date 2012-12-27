f6b076b2993250223746c607725492bc7bca1760
Cheng Ning <cn2005nian@163.com>
2012-12-27 16:45:45 +0800

    service base msysgit & TortoiseGit

---
+++ GitHelper/MSysGit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHelper
+{
+    /// <summary>
+    /// 说明：基于只有Msysgit软件下所提供的服务
+    /// 作者：ChengNing
+    /// 日期：2012-12-27
+    /// </summary>
+    public class MSysGit
+    {
+    }
+}
---
+++ GitHelper/TortoiseGit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHelper
+{
+    /// <summary>
+    /// 说明:基于系统已经安装的TortoiseGIT来提供服务。
+    /// 作者：ChengNing
+    /// 日期：2012-12-27
+    /// </summary>
+    public class TortoiseGit
+    {
+    }
+}
