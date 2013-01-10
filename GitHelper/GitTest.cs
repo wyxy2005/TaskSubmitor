f162a4b789f2bf418145860dd68fa3b9768c8b66
Cheng Ning <cn2005nian@163.com>
2013-01-10 16:15:50 +0800

    封装对msysgit的支持，提供接口

--- GitHelper/GitTest.cs
+++ GitHelper/GitTest.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using LibGit2Sharp;
-using LibGit2Sharp.Core;
 using System.Diagnostics;
+using NGit.Api;
+using NGit;
+using System.IO;
+using Sharpen;
+
 namespace GitHelper
 {
    
@@ -13,33 +16,66 @@ namespace GitHelper
     {
         public static void Test()
         {
-            Repository repo = new Repository(@"F:\git\libgit2sharp");
-
-
-            TreeChanges changes = repo.Diff.Compare(repo.Head.Tip.Tree,
-                DiffTargets.WorkingDirectory);
-
-            var expected = new StringBuilder()
-                .Append("diff --git a/file.txt b/file.txt\n")
-                .Append("index ce01362..4f125e3 100644\n")
-                .Append("--- a/file.txt\n")
-                .Append("+++ b/file.txt\n")
-                .Append("@@ -1 +1,3 @@\n")
-                .Append(" hello\n")
-                .Append("+world\n")
-                .Append("+!!!\n");
-
-            //Assert.Equal(expected.ToString(), changes.Patch);
-            Console.WriteLine(changes.Patch);
-
+            MSysGit git = new MSysGit(@"D:\test");
+            string re = git.Init();
+            Console.WriteLine(re);
         }
 
-        public static void TestDiff()
+        public static void TestNGit()
+        {
+            //FilePath directory = new FilePath(@"F:\git\test");
+            //Repository repo = new 
+            //InitCommand command = new InitCommand();
+            //command.SetDirectory(directory);
+            //Git git = command.Call();
+            //git.
+            //Git.Init();
+        }
+
+        public static void Init(string path)
+        {
+            Process tmerger = new Process();
+            tmerger.StartInfo.FileName = @"D:\Program Files\Git\cmd\git.exe";
+            tmerger.StartInfo.Arguments = @"init " + path;
+            tmerger.StartInfo.RedirectStandardOutput = true;//允许重定向输出
+            tmerger.StartInfo.RedirectStandardError = true;
+            tmerger.StartInfo.UseShellExecute = false;
+            tmerger.Start();
+            string result = tmerger.StandardOutput.ReadToEnd();
+            tmerger.WaitForExit();
+            tmerger.Close();
+            Console.WriteLine(result);
+        }
+
+        public static void TestTortoiseDiff()
         {
             Process tmerger = new Process();
             tmerger.StartInfo.FileName = @"D:\Program Files\TortoiseGit\bin\TortoiseMerge.exe";
             tmerger.StartInfo.Arguments = @"D:\AB-G-Pro\src\TestOra.java D:\AB-G-Pro\src\FileControl.java";
             tmerger.Start();
+        }
+
+        private static void Testlibgit2sharp()
+        {
+
+            //Repository repo = new Repository(@"F:\git\libgit2sharp");
+
+
+            //TreeChanges changes = repo.Diff.Compare(repo.Head.Tip.Tree,
+            //    DiffTargets.WorkingDirectory);
+
+            //var expected = new StringBuilder()
+            //    .Append("diff --git a/file.txt b/file.txt\n")
+            //    .Append("index ce01362..4f125e3 100644\n")
+            //    .Append("--- a/file.txt\n")
+            //    .Append("+++ b/file.txt\n")
+            //    .Append("@@ -1 +1,3 @@\n")
+            //    .Append(" hello\n")
+            //    .Append("+world\n")
+            //    .Append("+!!!\n");
+
+            ////Assert.Equal(expected.ToString(), changes.Patch);
+            //Console.WriteLine(changes.Patch);
         }
     }
 }
--- GitHelper/MSysGit.cs
+++ GitHelper/MSysGit.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.IO;
 
 namespace GitHelper
 {
@@ -12,5 +14,77 @@ namespace GitHelper
     /// </summary>
     public class MSysGit
     {
+        private const string GIT_EXE = @"D:\Program Files\Git\cmd\git.exe";
+        private string repoWorkPath;
+
+        public MSysGit(string repoWorkPath)
+        {
+            this.repoWorkPath = repoWorkPath;
+        }
+        /// <summary>
+        /// 执行GIT程序,调用msysgit
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        private string ExeGit(string arg)
+        {
+            string result = "";
+            Process tmerger = new Process();
+            //try
+            //{
+            tmerger.StartInfo.FileName = GIT_EXE;
+            tmerger.StartInfo.Arguments = arg;
+            tmerger.StartInfo.RedirectStandardOutput = true;//允许重定向输出
+            tmerger.StartInfo.RedirectStandardError = true;
+            tmerger.StartInfo.UseShellExecute = false;
+            tmerger.StartInfo.WorkingDirectory = this.repoWorkPath;
+            tmerger.Start();
+            result = tmerger.StandardOutput.ReadToEnd();
+            tmerger.WaitForExit();
+            //}
+            //catch (Exception ex)
+            //{
+            //result = ex.TargetSite.Name + "error";
+            //}
+            //finally
+            //{
+            tmerger.Close();
+            //}
+            return result;
+        }
+
+        public string Init()
+        {
+            string result = "";
+            if (string.IsNullOrEmpty(this.repoWorkPath))
+                result = "没有指定版本库目录";
+            else if (!Directory.Exists(this.repoWorkPath))
+                Directory.CreateDirectory(this.repoWorkPath);
+            result = ExeGit(@"init");
+            return result;
+        }
+
+        public string Commit(string comment)
+        {
+            string result = "";
+            result = ExeGit(@"commit -m '" + comment + "'");
+            return result;
+        }
+
+
+        public string Add()
+        {
+            return "";
+        }
+
+        public string Diff(string file)
+        {
+            return ExeGit("diff " + file);
+        }
+
+        public string DiffFileList()
+        {
+            return ExeGit("diff --stat");
+        }
     }
 }
