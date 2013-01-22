a0731dc5908ac6c2046c849f3156756b903926b8
Cheng Ning <cn2005nian@163.com>
2013-01-22 16:59:05 +0800

    添加界面通用的工具类，调整树节点的排序，使用lambda方法，同时添加linq方式对于对个字段的排序。

---
+++ UIForm/UiUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UIForm
+{
+    public class UiUtil
+    {
+        /// <summary>
+        /// 判断所需要的临时文件是否存在
+        /// </summary>
+        /// <returns></returns>
+        public static bool ExistTempDir()
+        {
+            if (Directory.Exists(sys.Default.TempDir))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 创建临时目录。判断如果不存在sys.Default.TempDir的处理方案
+        /// </summary>
+        public static void CreateTempDir()
+        {
+            if (ExistTempDir())
+                return;
+            else 
+            {
+                try
+                {
+                    Directory.CreateDirectory(sys.Default.TempDir);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
