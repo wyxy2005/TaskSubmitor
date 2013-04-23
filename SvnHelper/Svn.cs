21a00f88eb9cc07ff983194add737609c1ac8b28
Cheng Ning <cn2005nian@163.com>
2013-04-23 18:18:54 +0800

    add project SvnHelper.csproj

---
+++ SvnHelper/Svn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SvnHelper
+{
+    /// <summary>
+    /// 封装SVN操作
+    /// </summary>
+    public class Svn
+    {
+        private string repoUrl;
+        private string workSpacePath;
+
+        public Svn()
+        { 
+        }
+
+        public Svn(string workspacePath)
+        { 
+        }
+
+        public void Commit()
+        { 
+        }
+
+        public void Diff()
+        { 
+        }
+
+        public void Diff(string filePath)
+        { 
+        }
+
+        public void Log()
+        { 
+        }
+
+        public void Log(string filePath)
+        { 
+        }
+    }
+}
---
+++ SvnHelper/SvnTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SharpSvn;
+
+namespace SvnHelper
+{
+    public class SvnTest
+    {
+        public static void Test()
+        {
+            SvnTest test = new SvnTest();
+        }
+
+        private void SvnHello()
+        {
+            using (SvnClient client = new SvnClient())
+            {
+                string repoURL = @"http://localhost:8080/svn/test";
+                string workspace = @"D:\svntest\";
+                SvnInfoEventArgs info;
+                client.GetInfo(new Uri(repoURL),out info);
+                Console.WriteLine(info.Revision.ToString());
+
+                SvnImportArgs import = new SvnImportArgs();
+                import.LogMessage = "import test";
+                client.Import(workspace + "test.txt",new Uri(repoURL),import);
+
+                client.Update(workspace);
+            }
+ 
+        }
+    }
+}
